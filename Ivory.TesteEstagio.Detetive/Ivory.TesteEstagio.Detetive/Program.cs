b01278b13462f5b0dab0c6828b4dee71ce730a66
Douglas Abdo <douglas121996@gmail.com>
2019-05-23 00:31:25 -0300

    Update Program.cs
    Implementção do meu código no programa.

--- Ivory.TesteEstagio.Detetive/Ivory.TesteEstagio.Detetive/Program.cs
+++ Ivory.TesteEstagio.Detetive/Ivory.TesteEstagio.Detetive/Program.cs
@@ -14,6 +14,48 @@ namespace Ivory.TesteEstagio.Detetive
 
             // Escreva seu código aqui.
 
+            int aux = 1; //Flag
+
+            do
+            {
+                Console.Clear();
+
+                try
+                {
+                    Console.Write("Suspeito: ");
+                    int criminoso = int.Parse(Console.ReadLine());
+
+                    Console.Write("\nLocal: ");
+                    int local = int.Parse(Console.ReadLine());
+
+                    Console.Write("\nArma: ");
+                    int arma = int.Parse(Console.ReadLine());
+
+                    aux = testemunha.Interrogar(criminoso, local, arma);
+
+                    Console.WriteLine("\nRetorno: {0}", aux);
+
+                    if (aux != 0)
+                    {
+                        if (aux == 3)
+                            Console.WriteLine("Arma incorreta.");
+                        else if (aux == 2)
+                            Console.WriteLine("Local incorreto.");
+                        else if (aux == 1)
+                            Console.WriteLine("Assassino incorreto.");
+                    }
+                    else
+                        Console.WriteLine("\nVocê acertou!\n");
+                    
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape && aux != 0);
+
             Console.WriteLine("Número de Tentativas: " + testemunha.ObterNumeroInterogatoriosRealizados());
             Console.ReadKey();
         }
